fa261051485076e2978227aca1e4ee30439dcca1
Lasse Gresi <lasse.gresista@gmail.com>
2019-06-12 16:05:26 +0200

    Plots funktionieren, Eigenschwingung als Anfang

--- rungekutta/main_runge.jl
+++ rungekutta/main_runge.jl
@@ -3,29 +3,34 @@ using DelimitedFiles
 
 function main()
     #Initialize parameters
-    N = 10      #Number of atoms
+    N = 20      #Number of atoms
     α = 1.0     #harmonic coupling
-    β = 0.0       #anharmonic coupling
+    β = 0       #anharmonic coupling
 
-    t = 2       #time to solve the equation for
-    Δt = 0.1    #timestep width
+    t = 100      #time to solve the equation for
+    Δt = 0.0001    #timestep width
     nstep=Int(t/Δt)
 
     filename="./rungekutta/data/test" #Output filename
     #Starting conditions
     u0=zeros(2*N) #u0[1:N]: x0[1:N],   u0[N+1:2N]=v0
-    u0[2]=1
+    x=range(0, N-1; step=1)
+    u0[1:N]=sin.(π/(N-1)*x)
+
 
     #Configuration
     cfg = config(N, α, β, u0)
     #timestep(Δt, cfg)
     #Solve equation and output to file
     io_y = open(filename*"_y", "w") #Output cfg.y
-    write(io_y, "Parameters in first line (N, alpha, beta, t_max, delta t). Solution after second line( u_1(t1), ..., u_N(t1)) \n") #Header
-    writedlm(io, [N, α, β, t, Δt]') #Parameters
-
+    write(io_y, "(u_1(t1), ..., u_N(t1)) \n") #Header
     io_E = open(filename*"_E", "w") #Output Energies
     write(io_E, "t | E_total | E_kin |E_pot \n")
+
+    io_par = open(filename*"_p", "w")
+    write(io_par, "N, alpha, beta, t, deltat \n")
+    writedlm(io_par, [N, α, β, t, Δt]') #Parameters
+    close(io_par)
 
     for i in 1:nstep
         t_sim = (i-1)*Δt
@@ -35,6 +40,7 @@ function main()
         timestep(Δt, cfg) #Calculate cfg.y(t+Δt)
     end
     close(io_y)
+    close(io_E)
 end
 
 main()
--- rungekutta/ploting.jl
+++ rungekutta/ploting.jl
@@ -1,18 +1,25 @@
+using Plots
+using DelimitedFiles
+
+theme(:juno)
+
 xreso = 1280
 yreso = 720
 
+filename="./rungekutta/data/test"
 
-sol=readdlm("./rungekutta/data/test_y", '\t', Float64, '\n'; skipstart=2)
-energydata = readdlm("./rungekutta/data/test_E", '\t', Float64, '\n'; skipstart=1)
-time=
-E_full=
-T=
-Pot=
+sol=readdlm(filename*"_y", '\t', Float64, '\n'; skipstart=1)
+energydata = readdlm(filename*"_E", '\t', Float64, '\n'; skipstart=1)
+zeit = energydata[:,1]
+E_full= energydata[:,2]
+T= energydata[:,3]
+Pot= energydata[:,4]
 
-@gif for t in 1:M
-    t_sim = t*T/M
+N, α, β, t, Δt=readdlm(filename*"_p", '\t', Float64, '\n'; skipstart=1)
+N=Int(N)
 
-    p1 = plot(sol(t_sim)[1:N], ylims = (-1,1),
+@gif for t in 1:200000:length(zeit)
+    p1 = plot(sol[t, 1:N], ylims = (-1,1),
     label = "Atoms",
     xlabel = "x",
     ylabel = "Displacement",
@@ -22,29 +29,27 @@ Pot=
     line = nothing,
     size=(xreso,yreso))
 
-    plotlength = 20
+    plotlength = 2000
     if t<plotlength+1
         tp = 1
     else
         tp = t-plotlength
     end
     xaxis = range(tp,stop = t,step = 1)
-    p2 = plot(xaxis,energyValues[tp:t], label = "Total Energy",
-    ylims = (0,0.3),
+    p2 = plot(xaxis,E_full[tp:t], label = "Total Energy",
+    ylims = (0,1.5),
     xlabel = "Time",
     ylabel = "Energy",
     legend = :bottom,
     size=(xreso,yreso))
-    plot!(xaxis,kinteticValues[tp:t], label = "Kintetic Energy")
-    plot!(xaxis,potentialValues[tp:t], label = "Potential Energy")
+    plot!(xaxis,T[tp:t], label = "Kintetic Energy")
+    plot!(xaxis, Pot[tp:t], label = "Potential Energy")
 
 
 
     plot(p1,p2,layout = (2,1))
 end
 
-
-@sync begin
-    @async TimeEvo(cfg_harm, t)
-    @async TimeEvo(cfg_anharm, t)
-end
+plot(zeit, E_full, label = "Total Energy", ylims = (0,1.5),xlabel = "Time",ylabel = "Energy", legend = :bottom, size=(xreso,yreso))
+plot!(zeit, T, label = "Kintetic Energy")
+plot!(zeit, Pot, label = "Potential Energy")
