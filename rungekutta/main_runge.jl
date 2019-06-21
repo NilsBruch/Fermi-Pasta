9e06dc50de93a2b20fc66ae03971b839819473c3
Lasse Gresi <lasse.gresista@gmail.com>
2019-06-21 10:01:19 +0200

    Added spectrum to main.jl and plot.jl.

--- rungekutta/main_runge.jl
+++ rungekutta/main_runge.jl
@@ -1,48 +1,60 @@
 include("structs_runge.jl")
 using DelimitedFiles
+using FFTW
 
 function main()
     #Initialize parameters
-    N = 100      #Number of atoms
+    N = 50      #Number of atoms
     α = 1.0     #harmonic coupling
-    β = 0.5       #anharmonic coupling
+    β = 0.0       #anharmonic coupling
 
-    t = 10      #time to solve the equation for
+    t = 300      #time to solve the equation for
     Δt = 0.0001    #timestep width
-    nstep=Int(t/Δt)
+    nstep=Int(t/Δt)  #Numer of timesteps
 
-    filename="rungekutta/data/malsehen" #Output filename
+    Δt_per_f=3000  #Timesteps per output/frame in gif
+
+
+    filename="rungekutta/data/spectrum_harmonic" #Output filename
     #Starting conditions
     u0=zeros(2*N) #u0[1:N]: x0[1:N],   u0[N+1:2N]=v0
     x=range(0, N-1; step=1)
     u0[1:N]=sin.(16*π/(N-1)*x)
 
+    cfg = config(N, α, β, u0) # Initialize configuration
 
-    #Configuration
-    cfg = config(N, α, β, u0)
-    #timestep(Δt, cfg)
-    #Solve equation and output to file
-    io_y = open(filename*"_y", "w") #Output cfg.y
-    write(io_y, "(u_1(t1), ..., u_N(t1)) \n") #Header
-    io_E = open(filename*"_E", "w") #Output Energies
-    write(io_E, "t | E_total | E_kin |E_pot \n")
-
-    io_par = open(filename*"_p", "w")
+    #Prepare output files:
+    io_par = open(filename*"_p", "w")  #Parameters
     write(io_par, "N, alpha, beta, t, deltat \n")
-    writedlm(io_par, [N, α, β, t, Δt]') #Parameters
+    writedlm(io_par, [N, α, β, t, Δt]')
     close(io_par)
 
+    io_y = open(filename*"_y", "w") #Displacements
+    write(io_y, "(u_1(t1), ..., u_N(t1)) \n") #Header
+
+    io_E = open(filename*"_E", "w") #Energies
+    write(io_E, "t | E_total | E_kin |E_pot \n") #Header
+
+    io_spec = open(filename*"_spec", "w") #Spectrum
+    write(io_spec, "Absoloute value of DFT of y[1:N-1] \n")
+
+
     for i in 1:nstep
-        if rem(i-1,2000) == 0
-            t_sim = (i-1)*Δt
+        if rem(i-1, Δt_per_f) == 0
+            t_sim = (i-1)*Δt  #Time in simulation
             T, Pot, E = Energy(cfg) #kinetic, potential and total energy
-            writedlm(io_y, cfg.y') #Write out y
-            writedlm(io_E, [t_sim, E, T, Pot]')
+            spec=abs.(fft(cfg.y[1:N-1]))
+
+            writedlm(io_y, cfg.y[1:N]') #Write out y
+            writedlm(io_E, [t_sim, E, T, Pot]')  #write out energy
+            writedlm(io_spec, spec[1:Int(N/2)+1]') #Write out spectrum
+
         end
         timestep(Δt, cfg) #Calculate cfg.y(t+Δt)
     end
     close(io_y)
     close(io_E)
+    close(io_spec)
 end
 
 main()
--- rungekutta/ploting.jl
+++ rungekutta/ploting.jl
@@ -4,9 +4,9 @@ using DelimitedFiles
 theme(:juno)
 
 xreso = 1280
-yreso = 720
+yreso = 1000
 
-filename="rungekutta/data/malsehen"
+filename="rungekutta/data/spectrum_harmonic"
 
 sol=readdlm(filename*"_y", '\t', Float64, '\n'; skipstart=1)
 energydata = readdlm(filename*"_E", '\t', Float64, '\n'; skipstart=1)
@@ -14,31 +14,44 @@ zeit = energydata[:,1]
 E_full= energydata[:,2]
 T= energydata[:,3]
 Pot= energydata[:,4]
+spec = readdlm(filename*"_spec", '\t', Float64, '\n'; skipstart=1)
+
+#for t in 1:length(zeit)
+#    for k in 1:length(spec[1,:])
+#        if
+
 
 N, α, β, t, Δt=readdlm(filename*"_p", '\t', Float64, '\n'; skipstart=1)
 N=Int(N)
 
-Δt_per_f=500
-fps=30
-t/Δt/Δt_per_f
-
-anim = @animate for t in 1:Δt_per_f:length(zeit)
-    p1 = plot(sol[t, 1:N], ylims = (-1,1),
+anim = @animate for t in 1:length(zeit)
+    #Plot 
+    p1 = plot(sol[t, :], ylims = (-1,1),
     label = "Atoms",
     xlabel = "x",
     ylabel = "Displacement",
     linewidth = 0,
     markershape = :hexagon,
-    markersize = 15,
+    markersize = 1,
     line = nothing,
     size=(xreso,yreso))
 
+    p2 = plot(spec[t, :],
+    xlabel="Wave number k",
+    ylabel="Occupation",
+    ylim=(0, 20),
+    line=nothing,
+    legend=nothing,
+    marker=:circle,
+    markersize=2)
+
+    fps=30
+    Δt_per_f=zeit[2]-zeit[1]
     plotlength = Δt_per_f*fps*Δt
 
-
-    p2 = plot(zeit[1:t],E_full[1:t], label = "Total Energy",
+    p3 = plot(zeit[1:t],E_full[1:t], label = "Total Energy",
     ylims = (0,E_full[1]+0.2),
-    xlims = (-plotlength*3/4+Δt*t, plotlength*1/4+Δt*t),
+    xlims = (-plotlength*9/10+zeit[t], plotlength*1/10+zeit[t]),
     xlabel = "Time",
     ylabel = "Energy",
     legend = :bottom,
@@ -46,10 +59,11 @@ anim = @animate for t in 1:Δt_per_f:length(zeit)
     plot!(zeit[1:t],T[1:t], label = "Kintetic Energy")
     plot!(zeit[1:t], Pot[1:t], label = "Potential Energy")
 
-    plot(p1,p2,layout = (2,1))
+    plot(p1,p2,p3,layout = (3,1))
+    println(t)
 end
 
-gif(anim, "first_gif.gif", fps = 30)
+gif(anim, "rungekutta/figures/thermalization_harmonic.gif", fps = 30)
 
 
 plot(zeit, E_full, label = "Total Energy", ylims = (0,1.5),xlabel = "Time",ylabel = "Energy", legend = :bottom, size=(xreso,yreso))
